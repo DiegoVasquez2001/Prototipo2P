c15bbe88bff328401f4792b6b748d361250efcb6
DiegoVasquez2001 <engdv2017@gmail.com>
2020-04-30 10:40:40 -0600

    AÑADIENDO CRUD DE BODEGAS, MARCAS Y LINEAS

--- Prototipo2P/src/Contenedor/FrmMantMarc.java
+++ Prototipo2P/src/Contenedor/FrmMantMarc.java
@@ -5,6 +5,14 @@
  */
 package Contenedor;
 
+import java.awt.HeadlessException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author diego
@@ -53,14 +61,34 @@ public class FrmMantMarc extends javax.swing.JInternalFrame {
         jLabel3.setText("Estatus Marca:");
 
         BtnAlta.setText("Alta");
+        BtnAlta.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                BtnAltaActionPerformed(evt);
+            }
+        });
 
         BtnBaja.setText("Baja");
+        BtnBaja.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                BtnBajaActionPerformed(evt);
+            }
+        });
 
         BtnCambio.setText("Cambio");
+        BtnCambio.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                BtnCambioActionPerformed(evt);
+            }
+        });
 
         jLabel4.setText("Código Marca:");
 
         BtnBuscar.setText("Buscar");
+        BtnBuscar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                BtnBuscarActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
@@ -141,6 +169,83 @@ public class FrmMantMarc extends javax.swing.JInternalFrame {
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void BtnBuscarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_BtnBuscarActionPerformed
+        try{
+            Connection cn = DriverManager.getConnection("jdbc:mysql://localhost/sic", "root", "informaticdv2016");
+            PreparedStatement pst = cn.prepareStatement("select * from marcas where codigo_marca = ?");
+            pst.setString(1, TxtBuscar.getText().trim());
+            
+            ResultSet rs = pst.executeQuery();
+            
+            if(rs.next()){
+                TxtCodMarc.setText(rs.getString("codigo_marca"));
+                TxtNomMarc.setText(rs.getString("nombre_marca"));
+                TxtEstMarc.setText(rs.getString("estatus_marca"));
+            } else {
+                JOptionPane.showMessageDialog(null, "MARCA NO REGISTRADA");
+            }
+            
+        }catch (HeadlessException | SQLException e){
+            JOptionPane.showMessageDialog(null, e.toString());
+        }
+    }//GEN-LAST:event_BtnBuscarActionPerformed
+
+    private void BtnAltaActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_BtnAltaActionPerformed
+        try{
+            Connection cn = DriverManager.getConnection("jdbc:mysql://localhost/sic", "root", "informaticdv2016");
+            PreparedStatement pst = cn.prepareStatement("insert into marcas values(?,?,?)");
+            
+            pst.setString(1, TxtCodMarc.getText().trim());
+            pst.setString(2, TxtNomMarc.getText().trim());
+            pst.setString(3, TxtEstMarc.getText().trim());
+            pst.executeUpdate();
+            JOptionPane.showMessageDialog(null, "REGISTRO REALIZADO!");
+            TxtCodMarc.setText("");
+            TxtNomMarc.setText("");
+            TxtEstMarc.setText("");
+        }catch (HeadlessException | SQLException e){
+            JOptionPane.showMessageDialog(null, e.toString());
+        }
+    }//GEN-LAST:event_BtnAltaActionPerformed
+
+    private void BtnBajaActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_BtnBajaActionPerformed
+        try {
+             Connection cn = DriverManager.getConnection("jdbc:mysql://localhost/sic", "root", "informaticdv2016");
+            PreparedStatement pst = cn.prepareStatement("delete from marcas where codigo_marca = ?");
+            
+            pst.setString(1, TxtBuscar.getText().trim());
+            pst.executeUpdate();
+            
+            TxtCodMarc.setText("");
+            TxtNomMarc.setText("");
+            TxtEstMarc.setText("");
+            TxtBuscar.setText("");
+            
+            JOptionPane.showMessageDialog(null, "REGISTRO REMOVIDO!");
+            
+        } catch (HeadlessException | SQLException e) {
+            JOptionPane.showMessageDialog(null, e.toString());
+        }
+    }//GEN-LAST:event_BtnBajaActionPerformed
+
+    private void BtnCambioActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_BtnCambioActionPerformed
+        try {
+            String ID = TxtBuscar.getText().trim();
+            
+            Connection cn = DriverManager.getConnection("jdbc:mysql://localhost/sic", "root", "informaticdv2016");
+            PreparedStatement pst = cn.prepareStatement("update marcas set nombre_marca = ?, estatus_marca = ? where codigo_marca = " + ID);
+            
+            pst.setString(1, TxtNomMarc.getText().trim());
+            pst.setString(2, TxtEstMarc.getText().trim());
+            pst.executeUpdate();
+            
+            JOptionPane.showMessageDialog(null, "MODIFICACION REALIZADA");
+            
+        } catch (HeadlessException | SQLException e) {
+            JOptionPane.showMessageDialog(null, e.toString());
+        }
+    }//GEN-LAST:event_BtnCambioActionPerformed
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
